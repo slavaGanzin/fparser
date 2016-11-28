4479b172e526947fc650e5149df9670496fcbf6c
slavaGanzin <slava.ganzin@gmail.com>
2016-11-29 00:07:19 +0200

    replace #2

--- steps/replace.js
+++ steps/replace.js
@@ -1,1 +1,2 @@
-module.exports = ([r, s, f='gim']) => map(replace(new RegExp(r, f), s))
+regex = r => new RegExp(split('/', r)[1], split('/', r)[2])
+module.exports = ([r, s]) => map(replace(regex(r), s))
