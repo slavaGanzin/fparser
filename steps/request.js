fbee84ab9e55ffb59ae43c0fefe50a01a084013f
slavaGanzin <slava.ganzin@gmail.com>
2016-11-30 23:58:13 +0200

    config wip

--- steps/request.js
+++ steps/request.js
@@ -2,8 +2,13 @@ needle = promisify('needle')
 libxml = require('libxmljs')
 
 module.exports = (options) => () => {
+  _debug =
   debug('request')(`${options.url}`)
+  console.log(options)
   
   return needle.request(options.method, options.url, options.data, options)
+  .then(when(() => ARGV.verbose > 1, tap(debug('html'))))
+  // .then(when(() => ARGV.verbose > 1, tap(compose(debug('html'), path('body')))))
+  
   .then(input => libxml.parseHtml(input.body, {errors: false}))
 }
