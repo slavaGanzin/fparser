c977479effb53d913212c00e6c3adfac7fad94c9
slavaGanzin <slava.ganzin@gmail.com>
2016-12-02 15:44:35 +0200

    promiseAllObject

--- steps/parallel.js
+++ steps/parallel.js
@@ -1,12 +1,11 @@
 const {run} = require('../lib/steps')
 
-pAll = x => Promise.all(values(x)).then(zip(keys(x))).then(fromPairs)
 
 headIfOneElement = when(x => x.length<2, head)
 
 module.exports = options =>
 flatMap(input =>
-  pAll(mapObjIndexed(
+  promiseAllObject(mapObjIndexed(
     steps => run({steps})(input).then(headIfOneElement)
   , options))
 )
