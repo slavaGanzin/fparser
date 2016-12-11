fa6637fae12e296b2c1778fc82e4965d1b74e110
slavaGanzin <slava.ganzin@gmail.com>
2016-12-11 22:29:11 +0200

    fix db

--- db/redis.js
+++ db/redis.js
@@ -1,21 +1,15 @@
 Redis = require('ioredis')
 redis = null
-preprocessor = require('../db/preprocessor')
 
 connect = unless(() => redis, tap(options => redis = new Redis(options)))
 actions = {
   save: ({key, id, data}) =>
-    redis.hset(key, id, data)
+    new Promise(r => redis.hset(key, id, data).then(x => r(data)))
 }
 
-dispatch = options => data =>
-  actions[options.action](merge(options, {data:
-    preprocessor(options, data)
-  }))
-    .catch(debug('fparser:redis'))
-
-
-module.exports = compose(
-  dispatch,
+module.exports = {
+  actions,
   connect
-)
+}
+
+global.finishHandlers.push(() => redis.quit())
--- steps/db.js
+++ steps/db.js
@@ -1,5 +1,16 @@
-module.exports = options =>
-  flatMap(require('../db/'+options.provider)(options))
+preprocessor = require('../db/preprocessor')
+
+dispatch = (options, actions) => data =>
+  actions[options.action](merge(options, {
+    data: preprocessor(options, data),
+    id: data[options.id]
+  }))
+  .then(() => data)
+
+module.exports = options => {
+  const {actions,connect} = require('../db/'+options.provider)
+  return flatMap(compose(dispatch(options, actions), connect))
+}
 
 // module.exports = options =>
 //   flatMap(
