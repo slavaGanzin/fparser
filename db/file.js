4c3d101fec44bd9497f4298a4c3f7a1481c468b2
slavaGanzin <slava.ganzin@gmail.com>
2017-01-16 22:49:21 +0200

    db.file

--- db/file.js
+++ db/file.js
@@ -1,13 +1,19 @@
 fs = promisify('fs')
 mkdirp = require('mkdirp')
 const {sep} = require('path')
-preprocessor = require('../db/preprocessor')
 
-module.exports = options => {
-  mkdirp(options.key)
-  return data =>
-    fs.writeFile(
-      options.key + sep + data[options.id],
-      preprocessor(options, data)
-    )
+actions = {
+  save: ({data, _data, key, id}) => {
+    mkdirp(key)
+    return new Promise(r => fs.writeFile(
+      key + sep + data[id],
+      _data
+    ).then(() =>r(data)))
+  },
+  get: ({key, id}) =>
+    new Promise(r => fs.readFile(key + sep + id, 'utf8').then(r)).then(JSON.parse)
 }
+            
+connect = identity
+
+module.exports =  {actions, connect}
