fd6ff2e218b30ec097d6c0632a8d17bce3e9f702
slavaGanzin <slava.ganzin@gmail.com>
2017-01-27 00:02:49 +0200

    keys

--- steps/db.js
+++ steps/db.js
@@ -1,16 +1,22 @@
 const preprocessor = require('../db/preprocessor')
 
 const getId = (data, id) =>
-  when(isNil, () => { throw `empty key ${id} in ${JSON.pretty(data)}` }, data[id])
+  when(isNil, () => {
+    throw `empty key ${id} in ${JSON.pretty(data)}`
+  }, data[id])
+
 
 const dispatch = (options, actions) => data =>
   actions[options.action](merge(options, {
     data,
     _data: preprocessor(options, data),
-    id: getId(data, options.id)
+    key: options.keyPrefix +
+      (options.key != '' ? getId(data, options.key) : ''),
+    id: options.idPrefix + getId(data, options.id)
   }))
 
 module.exports = options => {
   const provider = require('../db/'+options.provider)
-  return flatMap(compose(dispatch(options, provider.actions), provider.connect))
+  provider.connect(omit(['keyPrefix'], options))
+  return flatMap(dispatch(options, provider.actions))
 }
