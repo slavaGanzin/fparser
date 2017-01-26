09ef17bc1eb1e64f9d702a020dfb567cf66b045c
slavaGanzin <slava.ganzin@gmail.com>
2017-01-26 22:36:14 +0200

    + const

--- db/file.js
+++ db/file.js
@@ -1,21 +1,27 @@
-fs = promisify('fs')
-mkdirp = require('mkdirp')
+const fs = promisify('fs')
+const mkdirp = require('mkdirp')
 const {sep} = require('path')
 
-actions = {
-  save: ({data, _data, key, id}) => {
-    mkdirp(key)
-    return new Promise(r => fs.writeFile(
-      key + sep + data[id],
-      _data
-    ).then(() =>r(data)))
-  },
-  get: ({key, id}) =>
-    new Promise(r =>
-      fs.readFile(key + sep + id, 'utf8').then(r)
-    ).then(JSON.parse)
+const save = ({data, _data, key, id}) => {
+  mkdirp(key)
+  return new Promise(r => fs.writeFile(
+    key + sep + data[id],
+    _data
+  ).then(() =>r(data)))
 }
+
+const get = ({key, id}) =>
+  new Promise(r =>
+    fs.readFile(key + sep + id, 'utf8').then(r)
+  ).then(JSON.parse)
+
             
 connect = identity
 
-module.exports =  {actions, connect}
+module.exports =  {
+  actions:{
+    get,
+    save
+  },
+  connect
+}
--- db/preprocessor.js
+++ db/preprocessor.js
@@ -1,6 +1,7 @@
-preprocessors = {
+const preprocessors = {
   json: JSON.stringify,
   jsonPretty: JSON.pretty
 }
+
 module.exports = ({preprocessor}, data) =>
   preprocessors[preprocessor](data)
