b7275e05b578c28626ae38c31855afa415785971
slavaGanzin <slava.ganzin@gmail.com>
2017-01-26 22:36:59 +0200

    database fixes

--- db/redis.js
+++ db/redis.js
@@ -1,15 +1,23 @@
-redis = null
+let redis = null
 
-connect = unless(() => redis, tap(options => redis = new require('ioredis')(options)))
-actions = {
-  save: ({key, id, _data, data}) =>
-    new Promise(r => redis.hset(key, id, _data).then(() => r(data))),
-  get: ({key, id}) =>
-    new Promise(r => redis.hget(key, id).then(r)).then(JSON.parse)
-}
+const connect = unless(() => redis, tap(options => redis = new require('ioredis')(options)))
+
+const save = ({key, id, _data, data}) =>
+  new Promise(r => redis.hset(key, id, _data).then(() => r(data)))
+  
+const get = ({key, id}) =>
+  new Promise(r => redis.hget(key, id).then(r)).then(JSON.parse)
+  
+const has = ({key, id}) =>
+  new Promise(r => redis.hget(key, id).then(r)).then(Boolean)
+  
+const reject = ({key, id, data}) =>
+  has({key, id}).then(has => has ? null : data)
 
 module.exports = {
-  actions,
+  actions: {
+    save, get, has, reject
+  },
   connect
 }
 
