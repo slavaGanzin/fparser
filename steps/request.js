40654f0474c4bd6f36d90948a41e2456af7ec299
slavaGanzin <slava.ganzin@gmail.com>
2017-02-13 23:55:58 +0200

    idebig

--- steps/request.js
+++ steps/request.js
@@ -26,17 +26,17 @@ module.exports = options => {
   l = limit(options.limit)
   return skip => {
     if (head(skip)) {
-      // console.error('skipped', options.url)
+      debug('skipped')(options.url)
       return Promise.resolve([])
     }
     return l(() => needle.request(
       options.method, options.url, options.data, options
     )
-    .then(tap(() => console.error('request', options.url)))
+    .then(tap(() => debug('request')(options.url)))
     .then(logErrors)
     .then(parse)
     .then(tap(document => document.url = options.url))
     .then(coerceArray)
     .catch(x => console.log(options.url,x) || Promise.resolve([null]))
   )
-}}
+  }}
