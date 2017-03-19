1cc9c46501e6726724dbf23a8b2b6e41aed2d4fa
slavaGanzin <slava.ganzin@gmail.com>
2017-03-19 20:54:35 +0200

    fix parallel

--- steps/attr.js
+++ steps/attr.js
@@ -1,10 +1,12 @@
 const url = require('url')
 
 module.exports = attr => flatMap(el => {
+  if (el[attr]) return el[attr]
   if (!el.attr(attr)) return null
   let value = el.attr(attr).value()
 
-  if (attr == 'href' && value && !url.parse(value).host) value = url.resolve(el.doc().url, value)
+  if (attr == 'href' && value && !url.parse(value).host)
+    value = url.resolve(el.doc().url, value)
 
   return value
 })
--- steps/parallel.js
+++ steps/parallel.js
@@ -3,9 +3,8 @@ const {run} = require('../lib/steps')
 const MORE_THAN_ONE = 2
 const headIfOneElement = when(x => x.length < MORE_THAN_ONE, head)
 
-module.exports = options =>
-flatMap(input =>
+module.exports = options => flatMap(input =>
   promiseAllObject(mapObjIndexed(
-    steps => run({steps})(input).then(headIfOneElement)
+    steps => run({steps})([input]).then(headIfOneElement)
   , options))
 )
