976d1d36d0b20b5450132eeb1793f143f194b405
slavaGanzin <slava.ganzin@gmail.com>
2017-03-20 00:20:14 +0200

    thenify

--- db/file.js
+++ db/file.js
@@ -1,34 +1,27 @@
 const connect = identity
-const fs = promisify('fs')
+const fs = thenify('fs')
 const mkdirp = require('mkdirp')
-const {sep, basename, dirname} = require('path')
+const {sep, basename, dirname,join} = require('path')
 
 const dir = (key, id) =>
   key + sep + dirname(id).replace(/.*\/\//, '')
 const file = basename
-const full = (key, id) => dir(key, id) + sep + file(id)
+const full = (key, id) => join(dir(key, id), file(id))
 
 const save = ({data, pre, key, id}) => {
   mkdirp.sync(dir(key, id))
-  return new Promise((r, rej) =>
-    fs.writeFile(full(key, id), pre(data))
-      .then(always(r(data)))
-      .catch(rej)
-)
+  return fs.writeFile(full(key, id), pre(data))
+    .then(always(data))
 }
 
 const get = ({key, id, post}) =>
-  new Promise(r =>
-    fs.readFile(full(key, id), 'utf8')
+  fs.readFile(full(key, id), 'utf8')
     .then(post)
-    .then(r)
-  )
-
 
 const has = ({key, id}) =>
-  new Promise(r => fs.stat(full(key, id))
-    .then(() => r(true))
-    .catch(() => r(false)))
+  fs.stat(full(key, id))
+    .then(always(true))
+    .catch(() => Promise.resolve(false))
 
 const skip = arg =>
   has(arg).then(_has => _has ? Promise.resolve(_has) : save(arg))
