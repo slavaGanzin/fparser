1f501923fcffab4e2734f106bb563346d9e78b31
slavaGanzin <slava.ganzin@gmail.com>
2017-03-21 01:11:24 +0200

    attr & document

--- steps/attr.js
+++ steps/attr.js
@@ -1,12 +1,12 @@
 const url = require('url')
 
+const resolveIfUrl = when(
+  (value, attr) => attr == 'href' && value && !url.parse(value).host,
+  value => url.resolve(el.doc().url, value)
+)
+
 module.exports = attr => flatMap(el => {
   if (el[attr]) return el[attr]
   if (!el.attr(attr)) return null
-  let value = el.attr(attr).value()
-
-  if (attr == 'href' && value && !url.parse(value).host)
-    value = url.resolve(el.doc().url, value)
-
-  return value
+  return resolveIfUrl(el.attr(attr).value())
 })
---
+++ steps/document.js
@@ -0,0 +1,1 @@
+module.exports = () => flatMap(el => el.doc())
