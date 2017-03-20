ad2c12f39312fb91e6b5cf828190c44d77c35917
slavaGanzin <slava.ganzin@gmail.com>
2017-03-20 23:30:10 +0200

    parallel: apply step config

--- steps/parallel.js
+++ steps/parallel.js
@@ -1,10 +1,10 @@
-const {run} = require('../lib/steps')
+const {runSteps} = require('../lib/steps')
 
 const MORE_THAN_ONE = 2
 const headIfOneElement = when(x => x.length < MORE_THAN_ONE, head)
 
 module.exports = options => flatMap(input =>
   promiseAllObject(mapObjIndexed(
-    steps => run({steps})([input]).then(headIfOneElement)
+    steps => runSteps(steps)([input]).then(headIfOneElement)
   , options))
 )
