fda4215d821444cf27260e9361628a9407057af0
slavaGanzin <slava.ganzin@gmail.com>
2017-03-23 15:00:12 +0200

    tests improve

--- steps/expect.js
+++ steps/expect.js
@@ -1,6 +1,4 @@
 const {expect} = require('chai')
 
-const e = compose(expect, tap(console.log))
-
 module.exports = expectations => data =>
-  mapObjIndexed((v, f) => e(data)[f](v), expectations) && data
+  mapObjIndexed((v, f) => expect(data)[f](v), expectations) && data
