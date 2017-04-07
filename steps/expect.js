79ca5a8dfb713ef8f75920ace7747c1b837bd7fc
slavaGanzin <slava.ganzin@gmail.com>
2017-04-07 21:45:15 +0300

    fix exect error styling

--- steps/expect.js
+++ steps/expect.js
@@ -1,4 +1,13 @@
 const {expect} = require('chai')
+const chalk = require('chalk')
 
 module.exports = expectations => data =>
-  mapObjIndexed((v, f) => expect(data)[f](v), expectations) && data
+  mapObjIndexed((v, f) => {
+    try {
+      expect(data)[f](v)
+    } catch (e) {
+      throw `Expect:
+${chalk.green(JSON.pretty(e.expected))}
+${chalk.red(JSON.pretty(e.actual))}`
+    }
+  }, expectations) && data
