f10e390faa356072029f623d98b428f71d0d8c0e
slavaGanzin <slava.ganzin@gmail.com>
2017-04-24 14:22:02 +0300

    chrome-dev

---
+++ steps/chrome.js
@@ -0,0 +1,26 @@
+throw 'TODO'
+const CDP = require('chrome-remote-interface')
+
+CDP(client => {
+    // extract domains
+  const {Network, Page} = client
+    // setup handlers
+
+  Network.requestWillBeSent(params => {
+    console.log(params.request.url)
+  })
+  Page.loadEventFired(() => {
+    client.close()
+  })
+    // enable events then start!
+  Promise.all([
+    Network.enable(),
+    Page.enable(),
+  ]).then(() => Page.navigate({url: 'https://github.com'})).catch(err => {
+    console.error(err)
+    client.close()
+  })
+}).on('error', err => {
+    // cannot connect to the remote endpoint
+  console.error(err)
+})
