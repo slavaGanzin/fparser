d65075cff66944aeca3ff51f9265f39689081c51
slavaGanzin <slava.ganzin@gmail.com>
2017-05-21 15:04:38 +0300

    redis auto parse

--- db/redis.js
+++ db/redis.js
@@ -1,5 +1,6 @@
 let redis = null
 const {handlers} = require('../lib/finish')
+const parseIfJSON = when(require('is-json'), JSON.parse)
 
 const connect = unless(() => redis, tap(options => {
   redis = new require('ioredis')(options)
@@ -7,11 +8,11 @@ const connect = unless(() => redis, tap(options => {
 }))
 
 const save = ({key, id, data}) =>
-  redis.hset(key, id, data)
+  redis.hset(key, id, when(is(Object), JSON.stringify, data))
     .then(() => `${key}/${id}`)
 
 const get = ({key, id}) =>
-  redis.hget(key, id)
+  redis.hget(key, id).then(parseIfJSON)
 
 const has = composeP(Boolean, get)
 
@@ -19,7 +20,7 @@ const skip = arg =>
   has(arg).then(_has => _has ? _has : save(arg))
 
 const all = ({key}) =>
-  redis.hgetall(key)
+  redis.hgetall(key).then(parseIfJSON)
 
 module.exports = {
   actions: {
