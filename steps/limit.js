6cd94cfbab25e8370069f846d39875104c11615a
slavaGanzin <slava.ganzin@gmail.com>
2017-05-25 14:43:33 +0300

    limit

--- steps/limit.js
+++ steps/limit.js
@@ -1,23 +1,5 @@
 const {run} = require('../lib/steps')
+const limit = require('promise-limit')
 
-const LIMIT_TIMEOUT = 100
-const START = 0
-
-const limit = (lim, i = START) => f =>
-  new Promise((resolve, reject) => {
-    const _limit = () => i < parseInt(lim)
-      ? tap(debug('limit'), ++i)
-        && f()
-          .then(tap(() => debug('limit')(--i)))
-          .then(resolve)
-          .catch(reject)
-      : setTimeout(_limit, LIMIT_TIMEOUT)
-
-    _limit()
-  })
-
-module.exports = options =>
-  compose(
-    limit(options.limit),
-    flatMap(run(options.steps))
-  )
+module.exports = options => args =>
+  limit(options.limit).map(flatten(args), run(options.steps))
