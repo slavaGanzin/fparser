05136d732c8a035cac641fba1cf9c58fda39cf16
Slava Ganzin <slava.ganzin@gmail.com>
2019-11-18 16:12:25 +0200

    lj / medium / paulgraham

--- steps/attr.js
+++ steps/attr.js
@@ -6,12 +6,4 @@ module.exports = attrs => flatMap(el => reduce((a, attr) => {
   if (el[attr]) return el[attr]
 
   return $(el).attr(attr)
-  //
-  // if (el[attr]) return el[attr]
-  // if (!is(Function, el.attr) || !el.attr(attr)) return null
-  // const value = el.attr(attr).value()
-  //
-  // if (attr == 'href' && value && !url.parse(value).host)
-  //   return url.resolve(el.doc().url, value)
-  // return value
 }, null, coerceArray(attrs)))
