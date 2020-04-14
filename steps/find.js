72f603cc5c903cf3b55304194191cd746a8c576e
Iurii Golskyi <igolskyi@gmail.com>
2020-04-14 20:02:15 +0300

    steps/find.js fix

--- steps/find.js
+++ steps/find.js
@@ -8,6 +8,20 @@ const c2x = unless(test(/^\/\//), require('css-to-xpath'))
 //   }, null, coerceArray(selectors))
 // )
 
-module.exports = selector => flatMap(xmldoc =>
-  xmldoc.find(c2x(join(', ', coerceArray(selector))))
-)
+// module.exports = selector => flatMap(xmldoc =>
+//   xmldoc.find(c2x(join(', ', coerceArray(selector))))
+// )
+
+module.exports = selectors => flatMap(xmldoc => {
+  const arr = coerceArray(selectors);
+  let result = xmldoc
+
+  for (let i = 0; i < arr.length; i++) {
+    const e = xmldoc.find(c2x(arr[i]))
+    if (e.length) {
+      result = e
+      break
+    }
+  }
+  return result;
+})
