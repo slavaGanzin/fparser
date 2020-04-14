d7f4c845af93d184240a796ef83920008ad34253
Slava Ganzin <slava.ganzin@gmail.com>
2020-04-14 17:37:51 +0300

    fix attr.js

--- steps/attr.js
+++ steps/attr.js
@@ -1,9 +1,4 @@
-const $ = require('cheerio')
-
 module.exports = attrs => flatMap(el => reduce((a, attr) => {
   if (a) return a
-
-  if (el[attr]) return el[attr]
-
-  return $(el).attr(attr)
+  if (el.attr(attr)) return el.attr(attr).value()
 }, null, coerceArray(attrs)))
