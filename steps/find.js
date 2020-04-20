cfefe32280a45b63f53c5eb0a2d254b2e1334022
Slava Ganzin <slava.ganzin@gmail.com>
2020-04-20 22:50:38 +0300

    xmldoc.get  -> xmldoc.find

--- steps/find.js
+++ steps/find.js
@@ -2,7 +2,7 @@ const c2x = unless(test(/^\/\//), require('css-to-xpath'))
 
 module.exports = selectors => flatMap(xmldoc => {
   for (const selector of coerceArray(selectors)) {
-    const e = xmldoc.get(c2x(selector))
+    const e = xmldoc.find(c2x(selector))
 
     if (e)
       return libxmljs.parseHtmlFragment(join('\n', e.childNodes()))
