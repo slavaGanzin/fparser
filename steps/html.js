5a9a769727e12e5357bacfec04285f5c6e906f9d
Slava Ganzin <slava.ganzin@gmail.com>
2020-07-02 18:24:50 +0300

    remove htmltidy

--- steps/html.js
+++ steps/html.js
@@ -1,8 +1,1 @@
-const {tidy} = require('htmltidy2')
-
-module.exports = options => flatMap(e => new Promise(r =>
-  tidy(e.toString('xhtml'), options, (e, result) => {
-    if (e) console.error(e)
-    r(trim(result))
-  })
-))
+module.exports = options => flatMap(e => e.toString(true))
