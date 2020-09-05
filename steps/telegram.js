d5c58525fcd9451a282b678868591358eaad5572
Slava Ganzin <slava.ganzin@gmail.com>
2020-09-05 18:38:23 +0300

    fix telegram timelimit

--- steps/telegram.js
+++ steps/telegram.js
@@ -3,8 +3,13 @@ const PH = require('../lib/placeholders')
 module.exports = options => {
   const bot = new (require('telegraf'))(options.token)
 
-  return flatMap(x =>
-    bot.telegram.sendMessage(options.chatId, PH.apply(x)(options.message), {parse_mode: 'Markdown'})
-      .catch(console.error)
-  )
+  return flatMap(x => {
+    const f = () => bot.telegram.sendMessage(options.chatId, PH.apply(x)(options.message), {parse_mode: 'Markdown'})
+      .catch(e => {
+        if (e.code == 429) setTimeout(f, Math.random()*5000)
+        else console.log(e)
+      })
+
+    return f()
+  })
 }
