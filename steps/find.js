4a7afbb00318071ec0905d57626ed37af287940a
Slava Ganzin <slava.ganzin@gmail.com>
2020-09-21 17:18:39 +0300

    cut debug

--- steps/find.js
+++ steps/find.js
@@ -2,7 +2,7 @@ const c2x = unless(test(/^\/\//), require('css-to-xpath'))
 
 module.exports = selectors => flatMap(xmldoc =>
   reduce((a, selector) => unless(isEmpty, x => {
-    // console.error(selector)
+    // console.log(selector)
     return reduced(x)
   }, xmldoc.find(c2x(selector))), null, coerceArray(selectors))
 )
