3f7cb42fae1c281c37f3cfc00fb696c5d3168dfe
Slava Ganzin <slava.ganzin@gmail.com>
2020-10-21 16:22:09 +0300

    regexp remove

--- steps/remove.js
+++ steps/remove.js
@@ -1,8 +1,26 @@
 const c2x = unless(test(/^\/\//), require('css-to-xpath'))
 
-module.exports = selectors => flatMap(
-  tap(xmldoc => map(selector => map(x => {
-    if (global.verbosity > 0) pp({[`remove ${selector}`]: x.toString()})
+const getSelectors = x =>
+  (x.attr('id') ? '#'+x.attr('id').value() : '') +
+  (x.attr('class') ? replace(/\s+/, '', replace(/\s(\w+)/gim, '.$1', ' '+x.attr('class').value())) : '')
+
+const traverse = xmldoc => regexes =>  {
+  const r = new RegExp(join('|', map(replace(/^\/|\/$/gim, ''), regexes)), 'gim')
+  return map(x => {
+    if (!r.test(getSelectors(x))) return
+    if (global.verbosity > 0) pp({[`remove ${r} ${getSelectors(x)}`]: x.toString()})
     x.remove()
-  }, xmldoc.find(c2x(selector))), coerceArray(selectors)))
+  }, xmldoc.find(c2x('*')))
+}
+
+const mapOverSelectors = xmldoc => map(selector => map(x => {
+  if (global.verbosity > 0) pp({[`remove ${selector}`]: x.toString()})
+  x.remove()
+}, xmldoc.find(c2x(selector))))
+
+module.exports = selectors => flatMap(
+  tap(xmldoc => evolve({
+    0: traverse(xmldoc),
+    1: mapOverSelectors(xmldoc)},
+    partition(x => x.startsWith('/'), coerceArray(selectors))))
 )
