07c67d4b9c8490e4375945f59d674bf8f049ef6b
Slava Ganzin <slava.ganzin@gmail.com>
2020-11-22 18:02:26 +0200

    rule based parser matching

--- steps/html.js
+++ steps/html.js
@@ -1,10 +1,9 @@
 
 const chrome = require('./chrome')
-const beautify = require('simply-beautiful');
 
 module.exports = options =>
   flatMap(e => {
     if (global.ARGV.chrome) chrome()([e.toString()])
 
-    return beautify.html(e.toString())
+    return simplyBeautiful.html(e.toString())
   })
