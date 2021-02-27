64381ef39bdd915560a31ab92f1376bbc1843ca2
Slava Ganzin <slava.ganzin@gmail.com>
2021-02-27 11:22:05 +0200

    improve match

--- steps/match.js
+++ steps/match.js
@@ -1,11 +1,8 @@
 const deepMap = flip(require('deep-map'))
 const r = memoize(r => new RegExp(r, 'gim'))
 
-module.exports = options => {
-  return flatMap(y => {
-    return pickBy((v,k) => !isEmpty(reject(isNil, values(v))), mapObjIndexed(pickBy(identity), deepMap((v,k) => {
-      const m = y.match(r(v))
-      if (m) return m
-    }, options)))
-  })
-}
+module.exports = options => flatMap(y => pickBy((v, k) => !isEmpty(reject(isNil, values(v))), mapObjIndexed(pickBy(identity), deepMap((v, k) => {
+  const m = y.match(r(v))
+
+  if (m) return m
+}, options))))
